c9506c45dbf48fa6358945fddaec99a167ba4ef9
sebastiankowalski2 <sebastian.kowalski20020@gmail.com>
2026-08-11 23:31:49 +0200

    fixed toasts

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Toaster
-        position="top-right"
+        position="top-center"
         toastOptions={{
           className: 'meetler-toast',
         }}
